d8f53abc9a6a7cc6f34fc311345af041bf670e06
tangtao <tangtao@itsource.cn>
2023-02-10 16:57:34 +0800

    变量使用

---
+++ day1/06-变量使用.py
@@ -0,0 +1,22 @@
+# 增：设置变量
+name = 'hello'
+
+# 查: 使用
+print(name)
+
+# 改: 修改变量值
+name = 'good'
+print(name)
+
+# 删
+# 语法：del 变量名
+del name
+# print(name)
+
+# 查看变量的内存地址
+# 语法：id(变量)
+str1 = 'hello'
+print(id(str1))
+
+# str1_cache = id(str1)
+# print(str1_cache)
