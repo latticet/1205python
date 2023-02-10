6d12e01938d352262563335eaa426383eec1c749
tangtao <tangtao@itsource.cn>
2023-02-10 16:12:49 +0800

    变量

---
+++ day1/04-变量.py
@@ -0,0 +1,23 @@
+# 需求1：输出一组字符串：AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+# 需求2：输出一组字符串：BAAAAAAAAAAAAAAAAAAAAAAAAAAA
+# 问题：1.代码冗余， 2.不容易维护
+print('BAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+print('BAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+print('AAAAAAAAAAAAAAAAAAAAAAAAAAA')
+print('BAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+print('==' * 20)
+
+# TODO 变量
+# 带名字的数据
+# 名字：变量名
+# 数目：变量值
+
+# 优化代码
+# 变量语法：
+# 变量名 = 变量值
+# 说明：将变量值赋值给变量名
+a = 'BAAAAAAAAAAAAAAAAAAAAAAAAAA'
+print(a)
+print(a)
+print(a)
+print(a)
