2f67987ca315a05cb0e68d33df3587251999b512
tangtao <tangtao@itsource.cn>
2023-02-11 10:58:41 +0800

    运算符

--- day2/01-输入.py
+++ day2/01-输入.py
@@ -5,10 +5,20 @@
 """
 1.阻塞代码继续向下运行
 2.输入内容回车继续向下运行
+3.input不管输入什么内容，返回的都是字符串类型
 """
-
-
+# 接收姓名
 name = input('姓名：')
 print(name)
+print(type(name))
+print('==' * 20)
 
-print(1111)
+# 接收年龄
+age = input('年龄：')
+print(age)
+print(type(age))
+
+# 体重
+weight = input('体重：')
+print(weight)
+print(type(weight))
