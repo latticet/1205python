585d46033815c5689f140d7de90caf83167c7dc4
tangtao <tangtao@itsource.cn>
2023-02-11 12:05:15 +0800

    运算符

--- day2/03-运算符.py
+++ day2/03-运算符.py
@@ -23,6 +23,64 @@ print('--' * 10)
 # 得到的结果永远是整数
 print(10 // 2)
 print(10 // 3)
+print('==' * 20)
 
 # TODO 比较运算符
+# ==
+print(10 == 10)
+print(10 == 11)
+print('--' * 20)
 
+# !=
+print(10 != 10)
+print(10 != 11)
+print('==' *20)
+
+# TODO 赋值运算符
+# +=
+a = 1
+b = 2
+a += b   # a = a + b
+print(a)  # 3
+
+a -= b   # a = a - b
+print(a)  # 1
+
+a *= b   # a = a * b
+print(a)
+print('--' * 20)
+
+b //= 2  # 1
+print(b)
+b /= 2   #
+print(b)
+
+x = 5
+x %= 2
+print(x)
+
+# TODO 身份运算符
+# 数据内存地址的比较
+str1 = 'hello'
+str2 = 'hello'
+print(str1 == str2)
+
+# is
+print(str1 is str2)
+print(id(str1))
+print(id(str2))
+
+num1 = 10000
+num2 = 10000
+
+print(num1 is num2)
+print('--' * 20)
+
+str3 = 'hello'
+str4_1 = 'he'
+str4_2 = 'llo'
+str4 = str4_1 + str4_2   # 字符串拼接
+print(str3 == str4)
+print(id(str3))
+print(id(str4))
+print(str3 is str4)
