7030f537b9022df55f7da6b55f5d2536830a51e5
tangtao <tangtao@itsource.cn>
2023-02-11 10:25:55 +0800

    数据类型

---
+++ day2/02-数据类型.py
@@ -0,0 +1,35 @@
+# 查看数据类型的函数
+# 语法：type(数据)
+
+# TODO 基础数据类型
+# 数字（number）int, float
+num1 = 10
+num2 = 1.1
+print(type(num1))
+print(type(num2))
+print('==' * 20)
+
+# 字符串 (str)
+# 说明：只要单引号或者双引号包裹的都是字符串
+# 单引号和双引号没有区别
+str1 = 'hello'
+str2 = "good"
+print(type(str1))
+print(type(str2))
+print('==' * 20)
+
+# 布尔类型 (bool)
+bool1 = True
+bool2 = False
+bool3 = 'True'
+print(type(bool1))
+print(type(bool2))
+print(type(bool3))
+
+# TODO 容器数据类型
+"""
+列表：list
+元组：tuple
+字典：dict
+集合: set
+"""
