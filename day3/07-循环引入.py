4892737362fbaa21d7d45f9d9d5c3b22e61dc48e
fine <zhangyaowenjy@163.com>
2023-02-13 14:35:12 +0800

    while循环基础

---
+++ day3/07-循环引入.py
@@ -0,0 +1,22 @@
+# 输出AAAAAAAAAAA
+print('BAAAAAAAAAA')
+print('AAAAAAAAAAA')
+print('AAAAAAAAAAA')
+print('AAAAAAAAAAA')
+print('AAAAAAAAAAA')
+
+# 变量
+a = 'BAAAAAAAAAA'
+print(a)
+print(a)
+print(a)
+print(a)
+print(a)
+
+# 1-100
+print(1)
+print(2)
+print(3)
+
+# 循环
+# 让程序按照我们的方式重复执行
