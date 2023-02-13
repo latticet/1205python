b8a86eef3acacf12bc8cd621162581f19054572e
fine <zhangyaowenjy@163.com>
2023-02-13 16:56:22 +0800

    循环嵌套

---
+++ day3/11-循环嵌套.py
@@ -0,0 +1,73 @@
+# TODO 语法
+"""
+while 条件语句:
+    while 条件语句:
+        循环体
+"""
+
+# TODO 练习
+# 需求1：
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+"""
+j = 0
+while j < 5:
+    i = 0
+    while i < 5:
+        print('*', end=' ')
+        i += 1
+    print()
+    j += 1
+"""
+# 外层循环控制星星的行数，内存循环控制星星个数
+
+# 需求2：
+"""
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+"""
+j = 1
+while j < 6:
+    i = 0
+    while i < j:
+        print('*', end=' ')
+        i += 1
+    print()
+    j += 1
+
+"""
+i = 0
+while i < 1:
+    print('*')
+    i += 1
+
+i = 0
+while i < 2:
+    print('*', end=' ')
+    i += 1
+print()
+
+i = 0
+while i < 3:
+    print('*', end=' ')
+    i += 1
+print()
+
+i = 0
+while i < 4:
+    print('*', end=' ')
+    i += 1
+print()
+
+"""
+
+# 需求3：99乘法表
+
