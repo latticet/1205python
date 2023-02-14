fd9fab8998838ba85b291082b8a8d70f22aab428
fine <zhangyaowenjy@163.com>
2023-02-14 10:52:22 +0800

    列表操作-添加

---
+++ day4/01-列表基础.py
@@ -0,0 +1,45 @@
+# 需求
+students_name = 'stu1, stu2, stu3, ....'
+
+name1 = 'xx'
+name2 = 'xx'
+name3 = 'xx'
+
+# 容器数据类型
+
+# 列表
+# TODO 数据结构
+"""
+# [item1, item2, ...]
+# item:元素
+"""
+# 定义
+# 字面量方式【常用】
+list1 = ['python', 'mysql']
+print(list1)
+
+# 构造函数方式 (类型转换)
+list2 = list('hello')
+print(list2)
+
+# 定义空列表
+list3 = list()
+print(list3)
+
+list4 = []
+print(list4)
+
+print('==' * 20)
+
+# TODO 特点
+"""
+1. 可以存储任何数据
+2. 元素可以重复
+3. 列表是有序的
+"""
+
+students_name1 = ['罗燕', '张杨', '李姗', '陈红', '张杨']
+
+list1 = [10, 10, 10.1, True, 'hello', ['a', 'b']]
+print(list1)
+
---
+++ day4/02-列表操作-增.py
@@ -0,0 +1,32 @@
+# 列表增删改查排序
+# TODO list.insert(索引， 数据)
+# 说明：指定索引位置添加数据
+# 索引(下标)：
+# 每一个元素都有自己的一个序号
+# 索引从左至右从0开始，从右至左从-1开始。
+list1 = ['python', 'mysql', 'linux', 'shell']
+list1.insert(0, 'git')
+print(list1)
+
+list1.insert(2, '禅道')
+print(list1)
+
+list1.insert(-1, '理论')
+print(list1)
+print('==' * 20)
+
+# TODO list.append(数据)
+# 说明：列表末尾追加数据
+list2 = ['python', 'mysql']
+list2.append('linux')
+print(list2)
+print('==' * 20)
+
+# TODO list.extend(容器)
+# 说明：将容器中的元素依次追加到列表末尾
+# 容器：list，str
+list3 = ['python', 'mysql']
+list3.extend('linux')
+print(list3)
+list3.extend(['shell', 'git'])
+print(list3)
