546fcbeaa6c32c89796aab8a3e4ff221ffc51ed7
fine <zhangyaowenjy@163.com>
2023-02-16 14:24:05 +0800

    字符串大小写转换

---
+++ day5/10-字符串-大小写转换.py
@@ -0,0 +1,14 @@
+# TODO string.upper()  将字母转换为大写
+print('hello'.upper())
+
+# TODO string.lower()  将字母转换为小写
+print('HELLO'.lower())
+
+# TODO string.title() 将字符串中所有单词的首字母大写
+print('hello world'.title())
+
+# TODO string.capitalize() 将字符串首字母大写
+print('hello world'.capitalize())
+
+# TODO string.swapcase() 反转大小写
+print('Hello World'.swapcase())
