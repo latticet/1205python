88ff732b1d7e58392ffe039042295655b309e252
fine <zhangyaowenjy@163.com>
2023-02-16 15:22:04 +0800

    字符串去除

---
+++ day5/12-字符串-去除空白字符.py
@@ -0,0 +1,13 @@
+# TODO * string.strip([str])  去除两边的str,不写str，去除的就是空格
+print(' hello    '.strip())
+print('123hello123'.strip('123'))
+print('==' * 20)
+
+# TODO * string.lstrip([str]) 去除左边的str。不写str，去除左边是空格
+print(' hello    '.lstrip())
+print('123hello123'.lstrip('123'))
+print('==' * 20)
+
+# TODO * string.rstrip([str]) 去除右边的str。不写str，去除右边是空格
+print(' hello    '.rstrip())
+print('123hello123'.rstrip('123'))
