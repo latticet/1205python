7ed4711605aaa58f2ed364c5dbc2d9883ecfedf8
fine <zhangyaowenjy@163.com>
2023-02-19 10:52:39 +0800

    函数返回值

---
+++ day7/03-函数返回值.py
@@ -0,0 +1,28 @@
+# 求2个数的和，并返回
+
+# 函数的返回值
+"""
+def 函数名([param1, param2, ...]):
+    函数体
+    return 函数要返回的结果（表达式）
+"""
+"""
+说明：
+1. 返回函数的运行结果
+2. return后的代码不执行
+"""
+
+
+def add1(a, b):
+    print(a + b)
+
+
+def add2(a, b):
+    return a + b  # 1 + 2  = 3
+    print(11111)
+
+
+print(add2(1, 2))  # print(3)
+
+result = add2(1, 2)  # result = 3
+print(result)
