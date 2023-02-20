6ce4b28e71465875c62ef6262aec3e6f6bc31bfd
fine <zhangyaowenjy@163.com>
2023-02-20 15:12:06 +0800

    文件写操作

--- day8/06-文件写操作.py
+++ day8/06-文件写操作.py
@@ -1,8 +1,9 @@
 # 打开文件
-# mode = w(write): 覆盖写。如果文件不存在，新建文件。如果原文件有内容，会覆盖。
+
+# TODO mode = w(write): 覆盖写。如果文件不存在，新建文件。如果原文件有内容，会覆盖。
 f = open('resource/demo2.txt', mode='w', encoding='utf8')
 
-# mode = a(append):追加写。如果文件不存在，新建文件。在原内容后面继续追加内容。
+# TODO mode = a(append):追加写。如果文件不存在，新建文件。在原内容后面继续追加内容。
 # f = open('resource/demo3.txt', mode='a', encoding='utf8')
 
 # 写入文件
