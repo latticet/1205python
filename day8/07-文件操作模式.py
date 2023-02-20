b32f9c1b7ddbdcde3a23abb866bfb474345f5cbc
fine <zhangyaowenjy@163.com>
2023-02-20 15:47:16 +0800

    文件操作模式

--- day8/07-文件操作模式.py
+++ day8/07-文件操作模式.py
@@ -3,13 +3,12 @@
 # a(append):追加写
 # b(bytes):二进制模式，不能单独使用，必须配合r、w、a使用
 # +(extension):扩展模式。不能单独使用，必须配合r、w、a使用。r+:读写模式。w+:读写。a+:读写。
-# 扩展模式会遵循主模式的特点
+# +扩展模式会遵循主模式的特点
 
 # 打开文件
 f = open('resource/demo4.txt', mode='w+', encoding='utf8')
 
 # 操作文件
-
 # 写入内容
 f.write('你好python')
 
