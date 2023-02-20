dbdfe2f10516362746e798528f93f2231eb51e88
fine <zhangyaowenjy@163.com>
2023-02-20 11:40:50 +0800

    文件操作基础

---
+++ day8/04-文件操作基础.py
@@ -0,0 +1,15 @@
+# TODO 1.打开文件
+# 语法：open(文件路径[, mode='r', encoding='utf8'])
+# 文件路径：要操作的文件位置，可以是绝对路径也可以是相对路径
+# mode：打开模式,默认就是r。r：read，w：write
+# encoding:编码格式，一般都写utf8
+# 返回：文件操作的资源句柄
+f = open('resource/demo1.txt', encoding='utf8')
+
+# TODO 2.操作文件
+# 读
+content = f.read()
+print(content)
+
+# TODO 3.关闭文件
+f.close()
