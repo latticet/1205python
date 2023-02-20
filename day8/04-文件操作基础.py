e1bb45111be3e91faa69eb99a17d9b06d54fea29
fine <zhangyaowenjy@163.com>
2023-02-20 11:51:44 +0800

    文件操作基础

--- day8/04-文件操作基础.py
+++ day8/04-文件操作基础.py
@@ -4,7 +4,10 @@
 # mode：打开模式,默认就是r。r：read，w：write
 # encoding:编码格式，一般都写utf8
 # 返回：文件操作的资源句柄
-f = open('resource/demo1.txt', encoding='utf8')
+# 相对路径
+# f = open('resource/demo1.txt', encoding='utf8')
+# 绝对路径
+f = open(r'E:\20221205软件测试\08.python阶段\1205python\day8\resource\demo1.txt', encoding='utf8')
 
 # TODO 2.操作文件
 # 读
