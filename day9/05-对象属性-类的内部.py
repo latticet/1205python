485f58f5fd7b5ab0d1c8a387b8eeffa0af438f91
fine <zhangyaowenjy@163.com>
2023-02-22 11:52:11 +0800

    对象属性-类的内部

--- day9/05-对象属性-类的内部.py
+++ day9/05-对象属性-类的内部.py
@@ -1,4 +1,49 @@
-# 增
-# 删
-# 改
-# 查
+# 对象属性类的内部操作
+# 类的内部->对象方法中
+
+# 定义类
+class Person:
+    # 定义对象方法
+    def set_info(self):
+        # TODO 增：设置属性
+        # 语法：self.attr = value
+        self.name = 'hello'
+        self.age = 18
+
+    def get_info(self):
+        # TODO 查询
+        print(self.name)
+        print(self.age)
+
+    def edit_info(self):
+        # TODO 修改
+        self.name = 'good'
+        self.age = 10
+
+    def del_info(self):
+        # TODO 删除
+        del self.name
+        del self.age
+
+
+# 创建对象
+p1 = Person()
+p1.set_info()
+
+print(p1.name)
+print(p1.age)
+print('==' * 20)
+
+# 调用查询方法
+p1.get_info()
+print('==' * 20)
+
+# 调用修改方法
+p1.edit_info()
+p1.get_info()
+
+# 调用删除
+p1.del_info()
+# p1.get_info()
+print(p1.name)
+print(p1.age)
