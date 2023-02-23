90a1c8ddf96d8fd448f5934219c5895899327f6b
fine <zhangyaowenjy@163.com>
2023-02-23 11:21:10 +0800

    object类

---
+++ day10/04-object类.py
@@ -0,0 +1,18 @@
+# object类是python中内置的类
+# 默认所有的类都继承自object类
+
+# 以下这三种定义类的方式都继承object
+
+# TODO 第一种[常用]
+class A:
+    pass
+
+
+# TODO 第二种
+class B():
+    pass
+
+
+# TODO 第三种
+class C(object):
+    pass
