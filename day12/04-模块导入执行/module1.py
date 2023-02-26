9ea48e5745904f8caf99c53ac1afce0b985bbf2e
fine <zhangyaowenjy@163.com>
2023-02-26 11:35:22 +0800

    模块导入时发生了什么

---
+++ day12/04-模块导入执行/module1.py
@@ -0,0 +1,13 @@
+def login():
+    print('登录功能')
+
+
+if __name__ == '__main__':
+    login()
+
+# print(__name__)
+# __name__:
+"""
+1. 如果py文件脚本方式执行：__main__
+2. 如果py文件导入方式执行：模块名
+"""
---
+++ day12/04-模块导入执行/module2.py
@@ -0,0 +1,9 @@
+name = 'module2'
+
+
+def fn1():
+    print('module2中的fn1函数')
+
+
+class Demo:
+    pass
