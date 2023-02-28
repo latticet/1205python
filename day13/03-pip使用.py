561b36a778b6072b5e2ef187be666d0486865d6f
fine <zhangyaowenjy@163.com>
2023-02-28 11:40:18 +0800

    pip使用

---
+++ day13/03-pip使用.py
@@ -0,0 +1,33 @@
+# TODO 安装
+# pip install 包名
+# pip install 包名==版本号
+"""
+pip install requests -i https://pypi.mirrors.ustc.edu.cn/simple
+pip install pytest
+pip install numpy
+pip install selenium
+"""
+
+# TODO 查看
+"""
+pip list
+"""
+
+# TODO 卸载
+"""
+pip uninstall 包名  
+"""
+
+# TODO 升级
+"""
+pip install -U 包名
+pip install --upgrade 包名
+"""
+
+# TODO 镜像
+# 拉取包的这个网站-python源
+# python镜像
+"""
+pip install 包名 -i 镜像网址  
+"""
+
